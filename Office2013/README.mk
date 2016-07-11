048b3a7d0eeea9baf7faf0e149ef00cfce7e2ca2
pantasio <hoang.lduc@gmail.com>
2016-07-11 13:55:06 +0700

    Create README.mk

---
+++ Office2013/README.mk
@@ -0,0 +1,7 @@
+Giao trinh
+http://www.mediafire.com/?o61obc4qoq4bp42
+http://www.mediafire.com/?l7tqfkeqjhqbmpq
+http://www.mediafire.com/?d2q3pj3s5u32ny1
+
+Bộ giáo trình Tin Học Office Cơ Bản (http://www.mediafire.com/download/u3vaxa6ose6wn5u/Final_Giao_Trinh_OFFICE2013_CoBan.docx), 259 trang
+Bộ giáo trình Tin Học Office Nâng Cao (http://www.mediafire.com/download/itd7y6fsfizb6bu/Final_Giao_Trinh_OFFICE2013_NangCao.docx), 267 trang
