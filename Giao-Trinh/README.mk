5f357761015756466b1c0a74246126752217477a
pantasio <hoang.lduc@gmail.com>
2016-07-11 14:49:14 +0700

    Create README.mk

---
+++ Giao-Trinh/README.mk
@@ -0,0 +1,217 @@
+#2 Xây dựng ứng dụng Android và IOS với DreamWeaver
+http://www.shopitc-vietnam.com/giao-trinh-video/300-xay-dung-ung-dung-android-va-ios-voi-dreamweaver.html
+  
+#2 Giáo trình video học Excel 2010 cơ bản 
+http://www.shopitc-vietnam.com/giao-trinh-video/310-giao-trinh-video-hoc-excel-2010-co-ban.html
+
+#2 Giáo trình video Chuyên sâu về Privot Table trong Excel 2010
+http://www.shopitc-vietnam.com/giao-trinh-video/311-privottable-chuyen-sau-excel-2010.html
+
+Thông tin mô tả :
+
+Thời lượng khóa học : 3h 43 phút.
+Trình độ : Intermediate.
+
+
+#2 Giáo trình video học Adobe Dreamweaver CS6 cơ bản
+http://www.shopitc-vietnam.com/giao-trinh-video/299-giao-trinh-video-hoc-adobe-dreamweaver-cs6-co-ban.html
+
+Thông tin mô tả :
+
+Thời lượng khóa học : 9h 24phút.
+Trình độ : Beginer.
+
+.#2 Giáo trình video Excel 2010 - định dạng bảng tính nâng cao
+http://www.shopitc-vietnam.com/giao-trinh-video/295-giao-trinh-video-excel-2010-dinh-dang-bang-tinh-nang-cao.html
+
+Thông tin mô tả :
+
+Thời lượng khóa học : 3h 2 phút.
+Trình độ : Intermediate.
+
+#2 Giáo trình video hiệu quả của HTML trong Email và Newsletters
+
+
+Thông tin mô tả :
+
+Thời lượng khóa học : 2h 24 phút.
+Trình độ : không phân biệt.
+
+#2 Giáo trình Video lập trình Python 3 Cơ bản
+
+
+Thông tin mô tả :
+
+Thời lượng khóa học : 6h 36 phút
+Trình độ : Beginer.
+
+#2 Giáo trình Video Adobe After Effects CS6 cơ bản
+http://www.shopitc-vietnam.com/giao-trinh-video/261-giao-trinh-video-adobe-after-effects-cs6-co-ban.html
+
+Thông tin mô tả :
+
+Thời lượng khóa học : 8h 41 phút
+Trình độ : Beginer.
+
+#2 Giáo trình video thủ thuật nâng cao với Google Adwords
+http://www.shopitc-vietnam.com/giao-trinh-video/260-thu-thuat-nang-cao-voi-google-adwords.html
+
+Thông tin mô tả :
+
+Thời lượng khóa học : 46 phút 54 giây
+Trình độ : Nâng cao.
+
+#2 Giáo trình Video phát triển ứng dụng Android với Java [cơ bản]
+http://www.shopitc-vietnam.com/giao-trinh-video/259-giao-trinh-video-phat-trien-ung-dung-android-voi-java.html
+
+Thông tin mô tả :
+
+Thời lượng khóa học : 5h 40 phút
+Trình độ : Beginer.
+
+#2 Giáo trình Video làm việc với Bootstrap Layouts : Thiết kế trang đơn Responsive
+http://www.shopitc-vietnam.com/giao-trinh-video/257-giao-trinh-video-lam-viec-voi-bootstrap-layouts-thiet-ke-trang-don-responsive.html
+
+Thông tin mô tả :
+
+Thời lượng khóa học : 3h 19 phút.
+Trình độ : Beginer.
+
+#2 Giáo trình Video học Google Adwords cập nhật 2015
+http://www.shopitc-vietnam.com/giao-trinh-video/253-giao-trinh-video-hoc-google-adwords-2015.html
+
+Thông tin mô tả :
+
+Thời lượng khóa học : 2h 51 phút
+Trình độ : Beginer.
+
+#2 Giáo trình Video làm việc với dữ liệu jQuery và AJAX
+http://www.shopitc-vietnam.com/giao-trinh-video/251-giao-trinh-video-lam-viec-voi-du-lieu-jquery-va-ajax.html
+
+Thông tin mô tả :
+
+Thời lượng khóa học : 28 phút 41 giây
+Trình độ : Intermediate .
+
+#2 Giáo trình Video Facebook cho Doanh Nghiệp
+http://www.shopitc-vietnam.com/giao-trinh-video/246-giao-trinh-video-facebook-cho-doanh-nghiep.html
+
+Thông tin mô tả :
+
+Thời lượng khóa học : 2h 45 phút
+Trình độ : Beginer.
+
+#2 Giáo trình Video Học NoSQL cho những người Pro SQL
+http://www.shopitc-vietnam.com/giao-trinh-video/243-giao-trinh-video-pro-trong-viec-su-dung-nosql.html
+
+Thông tin mô tả :
+
+Thời lượng khóa học : 3h 26 phút
+Trình độ : Intermediate .
+
+#2 Giáo trình Video tạo hình động animated tương tác với jQuery
+http://www.shopitc-vietnam.com/giao-trinh-video/241-giao-trinh-video-tao-hinh-dong-animated-tuong-tac-voi-jquery.html
+
+Thông tin mô tả :
+
+Thời lượng khóa học : 1h 17 phút
+Trình độ : Intermediate .
+
+#2 Giáo trình Video học Excel 2013 : Công thức và hàm nâng cao
+http://www.shopitc-vietnam.com/giao-trinh-video/240-giao-trinh-video-hoc-excel-2013-cong-thuc-va-ham-nang-cao.html
+
+Thông tin mô tả :
+
+Thời lượng khóa học : 5h 26 phút
+Trình độ : Advanced .
+
+#2 Giáo trình Video CSS với LESS và Sass
+http://www.shopitc-vietnam.com/giao-trinh-video/227-giao-trinh-video-css-voi-less-va-sass.html
+
+Thông tin mô tả :
+
+Thời lượng khóa học : 1h 57 phút
+Trình độ : Intermediate .
+
+#2 Giáo trình Video thủ thuật Excel 2013
+http://www.shopitc-vietnam.com/giao-trinh-video/226-giao-trinh-video-thu-thuat-excel-2013.html
+
+Thông tin mô tả :
+
+Thời lượng khóa học : 4h 34 phút
+Trình độ : Mọi trình độ
+
+#2 Giáo trình Video học sử dụng Google Drive
+http://www.shopitc-vietnam.com/giao-trinh-video/222-giao-trinh-video-hoc-su-dung-google-drive.html
+
+Thông tin mô tả :
+
+Thời lượng khóa học : 1h 29 phút
+Trình độ : Beginer.
+
+#2 Giáo trình Video cách tạo Plugins với jQuery
+http://www.shopitc-vietnam.com/giao-trinh-video/216-giao-trinh-video-cach-tao-plugins-voi-jquery.html
+
+Thông tin mô tả :
+
+Thời lượng khóa học : 1h 22 phút
+Trình độ : Intermediate .
+
+#2 Giáo trình Video học CSS và HTML trong 30 ngày
+http://www.shopitc-vietnam.com/giao-trinh-video/198-gia-o-tra-nh-video-ha-c-css-va-html-trong-30-nga-y.html
+
+Thông tin mô tả :
+
+Thời lượng khóa học : 5h với 33 bài học
+Trình độ : Beginer.
+
+
+#2 Giáo trình Video học jQuery trong 30 ngày
+http://www.shopitc-vietnam.com/giao-trinh-video/196-gia-o-tra-nh-video-ha-c-jquery-trong-30-nga-y.html
+
+Thông tin mô tả :
+
+Thời lượng khóa học : 8h với 29 bài học
+Trình độ : Beginer.
+
+#2 Giáo trình Video thiết kế web với SASS và COMPASS
+http://www.shopitc-vietnam.com/giao-trinh-video/192-giao-trinh-video-thiet-ke-web-voi-sass-va-compass.html
+
+Thông tin mô tả :
+
+Thời lượng khóa học : 19 bài học với 2,2 giờ 
+Trình độ : trung cấp.
+
+#2 Giáo trình Video javascript : Viết modular Javascript
+http://www.shopitc-vietnam.com/giao-trinh-video/191-giao-trinh-video-javascript-viet-modular.html
+
+Thông tin mô tả :
+
+Thời lượng khóa học : xem chi tiết link
+Trình độ : Intermediate.
+
+
+TO BE CONTINUED....
+http://www.shopitc-vietnam.com/giao-trinh-video.html?start=130
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
